19a924cddc7fe3567f87aba559ee5baadd02e6dd
Mustafa744 <mustafaashraf744@gmail.com>
2023-09-13 12:38:59 +0300

    ..

--- eval.py
+++ eval.py
@@ -9,5 +9,10 @@ dataset_info_path = vh.inputs('dataset_info').path()
 with open(dataset_info_path, 'r') as f:
     dataset_info = json.load(f)
     # print its content
+    print("loaded json")
     print(dataset_info)
+
+# save the dataset info to a txt file
+with open(vh.outputs().path("dataset_info.txt"), "w") as f:
+    f.write(str(dataset_info))
     
