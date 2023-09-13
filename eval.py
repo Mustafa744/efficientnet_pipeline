8da60aa850d0703c03d7c78c62ffe34c0e1ef6b0
Mustafa744 <mustafaashraf744@gmail.com>
2023-09-13 17:37:58 +0300

    ..

--- eval.py
+++ eval.py
@@ -31,62 +31,59 @@ train_tfrecord_file = vh.inputs("train").path()
 validate_tfrecord_file = vh.inputs("validate").path()
 test_tfrecord_file = vh.inputs("test").path()
 
-# Load a single example from the training dataset to determine the features
-example = next(tf.data.TFRecordDataset(train_tfrecord_file).take(1).as_numpy_iterator())
-example = tf.io.parse_single_example(example, features=None)
-features = {}
-for key in example.keys():
-    if example[key].dtype == 'float32':
-        features[key] = tf.io.FixedLenFeature([], dtype=tf.float32)
-    elif example[key].dtype == 'int64':
-        features[key] = tf.io.FixedLenFeature([], dtype=tf.int64)
+import tensorflow as tf
+import tensorflow_datasets as tfds
 
-# Define the shapes of your dataset
-shapes = {}
-for key in features.keys():
-    shapes[key] = ()
+class CustomTFRecordDataset(tfds.core.GeneratorBasedBuilder):
+    VERSION = tfds.core.Version("1.0.0")
+    RELEASE_NOTES = {"1.0.0": "Initial release"}
 
-# Load the training dataset
-train_dataset = tf.data.TFRecordDataset(train_tfrecord_file)
-train_dataset = train_dataset.map(lambda x: tf.io.parse_single_example(x, features))
+    def _info(self):
+        # Define the dataset metadata, including features and labels
+        features = {
+            'image': tfds.features.Image(shape=(28, 28, 1), dtype=tf.uint8),
+            'label': tfds.features.ClassLabel(num_classes=10),
+        }
+        return tfds.core.DatasetInfo(
+            builder=self,
+            description="Custom dataset from TFRecords",
+            features=tfds.features.FeaturesDict(features),
+        )
 
-# Load the validation dataset
-validate_dataset = tf.data.TFRecordDataset(validate_tfrecord_file)
-validate_dataset = validate_dataset.map(lambda x: tf.io.parse_single_example(x, features))
+    def _split_generators(self, dl_manager):
+        # Define how to download and extract the dataset
+        # You can specify the file paths or URLs for your TFRecord files here
 
-# Load the testing dataset
-test_dataset = tf.data.TFRecordDataset(test_tfrecord_file)
-test_dataset = test_dataset.map(lambda x: tf.io.parse_single_example(x, features))
+        splits = {
+            'train': train_tfrecord_file,
+            'validation': validate_tfrecord_file,
+            'test': validate_tfrecord_file,
+        }
 
-# Create a `tf.data.Dataset` object for each split
-train_dataset = train_dataset.batch(32)
-validate_dataset = validate_dataset.batch(32)
-test_dataset = test_dataset.batch(32)
+        return {split_name: tfds.core.SplitGenerator(
+                name=split_name,
+                gen_kwargs={'file_path': file_path},
+            ) for split_name, file_path in splits.items()}
 
-# Create a `tfds.features.FeaturesDict` object for the features
-features_dict = tfds.features.FeaturesDict(features)
+    def _generate_examples(self, file_path):
+        # Define how to parse the TFRecord files and yield examples
+        # Example:
+        for record in tf.data.TFRecordDataset(file_path):
+            # Parse the record and yield examples
+            # Example:
+            feature_dict = tf.io.parse_single_example(
+                record,
+                features={
+                    'image': tf.io.FixedLenFeature([], tf.string),
+                    'label': tf.io.FixedLenFeature([], tf.int64),
+                }
+            )
+            image = tf.image.decode_image(feature_dict['image'], channels=1)
+            label = feature_dict['label']
+            yield {
+                'image': image,
+                'label': label,
+            }
 
-# Create a `tfds.core.DatasetInfo` object for the dataset
-dataset_info = tfds.core.DatasetInfo(
-    builder=tfds.core.DatasetBuilder(),
-    description='My dataset description',
-    features=features_dict,
-    homepage='https://mydataset.com',
-    citation='My dataset citation',
-    metadata=tfds.core.MetadataDict(),
-    supervised_keys=('input', 'label'),
-    splits=tfds.core.SplitDict({
-        'train': tfds.core.SplitInfo(num_examples=len(train_dataset)),
-        'validation': tfds.core.SplitInfo(num_examples=len(validate_dataset)),
-        'test': tfds.core.SplitInfo(num_examples=len(test_dataset)),
-    }),
-    download_checksums={},
-)
-
-# Save the dataset info to a JSON file
-with open(vh.outputs().path('dataset_info.json'), 'w') as f:
-    f.write(dataset_info.as_json)
-
-# Save the features to a JSON file
-with open(vh.outputs().path('features.json'), 'w') as f:
-    f.write(features_dict.as_json)
+tfds.builder('custom_tfrecord_dataset')
+os.system("tfds build custom_tfrecord_dataset")
