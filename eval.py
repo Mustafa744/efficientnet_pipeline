6b737436d78c4c854b3b6904fd29a652a4675866
Mustafa744 <mustafaashraf744@gmail.com>
2023-09-13 18:47:11 +0300

    ..

--- eval.py
+++ eval.py
@@ -1,57 +1,57 @@
 import json
 import tensorflow as tf
 import tensorflow_datasets as tfds
-import valohai
+import valohai as vh
 
 # Define the paths to your TFRecord files using Valohai inputs
-train_tfrecord_file = valohai.inputs("train").path()
-validate_tfrecord_file = valohai.inputs("validate").path()
-test_tfrecord_file = valohai.inputs("test").path()
+train_tfrecord_file = vh.inputs("train").path()
+validate_tfrecord_file = vh.inputs("validate").path()
+test_tfrecord_file = vh.inputs("test").path()
 
-# Create a function to calculate metadata from TFRecord files
-def calculate_metadata(tfrecord_file):
+
+def calculate_metadata(tfrecord_path):
+    # Create a TFRecord dataset from the file
+    dataset = tf.data.TFRecordDataset(tfrecord_path)
+    
+    # Initialize an empty dictionary to store feature information
     features = {}
-    shapes = {}
-    
-    # Read TFRecord files to determine feature names and data types
-    dataset = tf.data.TFRecordDataset(tfrecord_file)
+
+    # Iterate through the dataset to extract feature information
     for record in dataset.take(1):
-        example = tf.io.parse_single_example(record, features)
-        for feature_name, feature_tensor in example.items():
-            features[feature_name] = feature_tensor.dtype.name
-            shapes[feature_name] = feature_tensor.shape.as_list()
-    
-    return features, shapes
+        example = tf.train.Example()
+        example.ParseFromString(record.numpy())
 
-# Calculate metadata for train, validate, and test TFRecord files
-train_features, train_shapes = calculate_metadata(train_tfrecord_file)
-validate_features, validate_shapes = calculate_metadata(validate_tfrecord_file)
-test_features, test_shapes = calculate_metadata(test_tfrecord_file)
+        for feature_name, feature in example.features.feature.items():
+            dtype = tf.dtypes.as_dtype(feature.WhichOneof('kind')).name
+            shape = feature.ListFields()[0][1].shape
 
-# Construct the metadata dictionary
-metadata = {
-    'pythonClassName': 'tensorflow_datasets.core.features.features_dict.FeaturesDict',
-    'featuresDict': {
-        'features': {
-            feature_name: {
+            features[feature_name] = {
                 'pythonClassName': 'tensorflow_datasets.core.features.feature.Feature',
                 feature_name: {
-                    'shape': {
-                        'dimensions': [str(dim) for dim in shape]
-                    },
                     'dtype': dtype,
-                }
+                    'shape': {
+                        'dimensions': [str(dim) for dim in shape],
+                    },
+                },
             }
-            for feature_name, dtype, shape in zip(
-                train_features.keys(), train_features.values(), train_shapes.values()
-            )
-        }
+
+    return features
+
+def generate_metadata(tfrecord_path, output_path):
+    # Calculate metadata for the TFRecord file
+    metadata = {
+        'pythonClassName': 'tensorflow_datasets.core.features.features_dict.FeaturesDict',
+        'featuresDict': {
+            'features': calculate_metadata(tfrecord_path),
+        },
     }
-}
+    with open(output_path, 'w') as metadata_file:
+        json.dump(metadata, metadata_file, indent=4)
 
-# Save the metadata to a JSON file at the Valohai output path
-with open(valohai.outputs().path('metadata.json'), 'w') as metadata_file:
-    json.dump(metadata, metadata_file, indent=4)
 
-# Print the generated metadata
-print(json.dumps(metadata, indent=4))
+
+# Example usage:
+tfrecord_path = train_tfrecord_file
+output_path = vh.outputs().path('metadata.json')
+generate_metadata(tfrecord_path, output_path)
+
